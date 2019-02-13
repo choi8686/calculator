c7c29d5d7c36cf6c7cf2cae41bd4b1fa97561d02
unknown <this0924@gmail.com>
2019-02-13 16:14:27 +0900

    deleted

--- calculate.js
+++ calculate.js
@@ -1,25 +1,67 @@
 var number = Array.from(document.getElementsByClassName('number'));
-var operator = document.getElementsByClassName('operator');
+
+var operator = Array.from(document.getElementsByClassName('operator'));
 var inputObj = document.getElementById('window');
+var inputOp = document.getElementById('op-window');
 var borrar = document.getElementById('delete');
+var resulta = document.getElementById('result');
+var equal = document.getElementById('eq-operator');
+
+var selectOperator;
+var firstNumber;
+var firstOperatorNumber;
+var secondNumber;
+var result;
 
 function touchNumber(){
 number.forEach((numberElement) => {
   numberElement.addEventListener('click', function(event){
+
     var inputValue = inputObj.value;
     inputValue += numberElement.value;
     inputObj.value = inputValue;
-    return numberElement.value;
+    firstNumber = inputObj.value;
+    var tt = parseInt(firstNumber);
+    firstNumber = tt;
 
-    if(value=="=")
   })
+      return numberElement.value;
 })
+
+}
+
+function getSecondnumber(){
+  if(firstOperatorNumber !== null){
+    inputObj.value = "";
+
+  }
+}
+
+function touchOperator(){
+  operator.forEach((operatorElement) =>{
+    operatorElement.addEventListener('click', function(event){
+      var inputValue = inputOp.value;
+      inputValue += operatorElement.value;
+      inputOp.value = inputValue;
+
+      selectOperator = inputOp.value;
+      firstOperatorNumber = inputOp.value;
+      console.log(typeof firstOperatorNumber)
+
+    })
+        return operatorElement.value;
+  })
+
+}
+function final(){
 
 }
 
 function reset(){
   borrar.addEventListener('click', function(event){
    inputObj.value = "";
+   inputOp.value = "";
+   result.value = "";
 });
 
 }
@@ -27,6 +69,8 @@ function reset(){
 function init(){
   touchNumber();
   reset();
-
+  touchOperator();
+  getSecondnumber();
+  final();
 }
 init();
